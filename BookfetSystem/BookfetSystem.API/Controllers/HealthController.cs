7c8a0ac17cd5e1479ae68ed044d8ad6c07797e7c
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-12-28 15:15:16 +0700

    init project

---
+++ BookfetSystem/BookfetSystem.API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookfetSystem.API.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("check-health")]
+        public IActionResult CheckHealth()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
