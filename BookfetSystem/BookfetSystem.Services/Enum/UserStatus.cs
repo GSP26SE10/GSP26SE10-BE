a0c0e16e9c128452ac3e0f56988cf29dae3a3ac3
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2026-02-15 22:24:37 +0700

    generate entities

---
+++ BookfetSystem/BookfetSystem.Services/Enum/UserStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookfetSystem.Services.Enum
+{
+    public enum UserStatus
+    {
+        ACTIVE = 1,
+        INACTIVE = 0
+    }
+}
