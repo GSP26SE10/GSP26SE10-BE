c232a04baf2a1d84382c0516274769e70314415b
huynqse183261 <huynqse183261@fpt.edu.vn>
2026-03-04 19:46:22 +0700

    Done FeedbackMenu

---
+++ BookfetSystem/BookfetSystem.Services/Interface/IFeedbackMenuService.cs
@@ -0,0 +1,15 @@
+using BookfetSystem.Services.Models.Common;
+using BookfetSystem.Services.Models.Request;
+using BookfetSystem.Services.Models.Response;
+using System.Threading.Tasks;
+
+namespace BookfetSystem.Services.Interface
+{
+    public interface IFeedbackMenuService
+    {
+        Task<PagedResponse<FeedbackMenuResponse>> GetAllFeedbackMenuFilteredAsync(FeedbackMenuFilterRequest request, int page, int pageSize);
+        Task<ApiResponse<FeedbackMenuResponse>> CreateAsync(FeedbackMenuCreateRequest request);
+        Task<ApiResponse<FeedbackMenuResponse>> UpdateAsync(int id, FeedbackMenuUpdateRequest request);
+        Task<ApiResponse<bool>> DeleteAsync(int id);
+    }
+}
