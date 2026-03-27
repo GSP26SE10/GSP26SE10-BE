34d1b9a8e0bf935456bfcec1ee19ee73d6274dfd
vmtn2210 <vmtan2210@gmail.com>
2026-03-27 17:04:35 +0700

    add envi github

--- BookfetSystem/BookfetSystem.Repositories/Entities/ExtraChargeCatalog.cs
+++ BookfetSystem/BookfetSystem.Repositories/Entities/ExtraChargeCatalog.cs
@@ -20,6 +20,7 @@ public partial class ExtraChargeCatalog
     public decimal? UnitPrice { get; set; }
 
     public string Status { get; set; }
+
     public DateTime? CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
